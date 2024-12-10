0678b45e1c9fda8a0b87b2657c6cfd2cbdf240d6
Tomás Alves <102360450+Tashmacher@users.noreply.github.com>
2024-12-10 17:20:57 +0000

    baseDB e sqlQuerys criadas e iniciadas.
    baseDB tem o codigo base para executar as querys sobre.
    sqlQuerys é onde estão as querys que deverão cumprir as requests do enunciado e terão as querys feitas a baseDB

---
+++ proj fase 2/sqlQuerys.sql
@@ -0,0 +1,69 @@
+--1
+/*
+Nickname e país de todos os jogadores que já obtiveram pelo menos dez pontos 
+em algum encontro realizado em Lisboa (LIS). O ano desses jogos também deverá 
+ser apresentado; e os resultados ordenados de forma ascendente por ano, e 
+descendente por país e nickname. Nota: pretende-se uma interrogação sem sub
+interrogações: apenas com um SELECT.
+*/
+
+--2
+/*
+Número, nome, nickname, papel e país dos jogadores que são Senior em pelo 
+menos um dos papeis: atirador e suporte, ou que tenham um nickname de 6 
+caracteres, começado por ‘S’ e terminado por ‘r’ e tenham começado a jogar antes 
+(*) do recente Torneio League of Legends realizado na Islândia (2021). Nota: 
+pode usar construtores de conjuntos.
+*/
+
+--3
+/*
+Identificação dos encontros de semi-finais realizados em Paris (PAR) em que 
+jogou, pelo menos, um jogador que iniciou actividade antes de 2021 e tem um 
+nickname contendo ‘Purple’.
+*/
+
+--4
+/*
+Nome, ano e país dos jogadores que nasceram antes do ano 2000, e que nunca 
+participaram como atirador, em finais com equipas da Dinamarca (DK).
+*/
+
+--5
+/*
+Identificação dos encontros das fases finais (SF e FF) em que tenham participado 
+jogadores islandeses (IS) junior em todos os papeis que existem. Nota: o resultado 
+deve vir ordenado pelo ano de forma descendente, e pela fase e número do 
+encontro de forma ascendente.
+*/
+
+--6
+/*
+Número de encontros em que jogou cada jogador, em cada papel. Nota: os 
+resultados devem ser ordenados, de forma ascendente, pelo nome e número do 
+jogador, e de forma descendente pelo papel. 
+*/
+
+--7
+/*
+Nome, número e país dos jogadores que participaram em mais finais (FF), em 
+cada papel. Notas: em caso de empate, devem ser mostrados todos os jogadores 
+em causa.
+*/
+
+--8
+/*
+Para cada ano de início de actividade, o número e nome do jogador que jogou em 
+mais encontros. Apresentar também o número total de encontros em que jogou, e 
+a maior e menor pontuação que obteve nesses encontros. Nota: em caso de empate 
+do total de encontros, mostrar todos os jogadores em causa. 
+*/
+
+--9
+/*
+Nickname, ano de nascimento e país dos jogadores que nasceram depois do mítico 
+campeão de League of Legends, Faker (o coreano Lee Sang-hyeok, nasc: 1996), 
+e que jogaram em menos de 4 encontros destes torneios de League of Legends, 
+mesmo que não tenham jogado em nenhum. Pretende-se uma interrogação sem 
+sub-interrogações: apenas com um SELECT. 
+*/
